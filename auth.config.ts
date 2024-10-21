f4ce25ce7734e239568dce11c4abf7840b80fc87
jenil-desai <jenildev91@gmail.com>
2024-10-21 22:18:16 +0530

    v.1.1.0

---
+++ auth.config.ts
@@ -0,0 +1,43 @@
+import { NextAuthConfig } from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+
+const authConfig = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID ?? "",
+      clientSecret: process.env.AUTH_GOOGLE_SECRET ?? "",
+    }),
+    CredentialProvider({
+      credentials: {
+        email: {
+          type: "email",
+        },
+        password: {
+          type: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        const user = {
+          id: "1",
+          name: "John",
+          email: credentials?.email as string,
+        };
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/",
+  },
+} satisfies NextAuthConfig;
+
+export default authConfig;
