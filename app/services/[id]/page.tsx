71970ca79d8956e9808625a7ca408b78930b1daa
jenil-desai <jenildev91@gmail.com>
2024-10-30 23:49:50 +0530

    v10.0.18

--- app/services/[id]/page.tsx
+++ app/services/[id]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { Timeline } from "@/components/ui/timeline";
 import fetcher from "@/fetcher";
-import { NextRequest } from "next/server";
 import useSWR from "swr";
 import { TimelineSkeleton } from "./skeleton";
+
+export const runtime = "edge";
 
 export default function ({ params }: { params: { id: string } }) {
   const { data, isLoading, error } = useSWR(`/api/services/${params.id}`, fetcher);
