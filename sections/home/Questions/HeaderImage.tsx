cc6c662a50bc5f6538bfff751854052360e1a8c9
Jenil Desai <jenildev91@gmail.com>
2024-12-10 12:49:43 +0530

    v12.2.2
    🧑🏻‍🔧 Changed Images Of Why Choose Sylvorn Section On Home Page.

--- sections/home/Questions/HeaderImage.tsx
+++ sections/home/Questions/HeaderImage.tsx
@@ -8,7 +8,7 @@ interface HeaderImageProps {
 export default function HeaderImage({ image, title }: HeaderImageProps) {
   return (
     <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl">
-      <img src={image} alt={title} className="w-full h-full rounded-xl object-cover object-center" />
+      <img src={image} alt={title} className="w-full h-full rounded-xl object-fit object-center" />
     </div>
   );
 }
