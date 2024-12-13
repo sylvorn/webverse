62be2f1f06a3ac01a1fd5120eaf6d55fc14cb26f
Jenil Desai <jenildev91@gmail.com>
2024-12-13 14:35:57 +0530

    v12.7.1
    ➕ Added Metadata For FAQs Page.
    🧑🏻‍🔧 Fixed Sitemap For Old Company Name.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://prayaminfosoft.com",
+  siteUrl: process.env.SITE_URL || "https://sylvorn.com",
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
